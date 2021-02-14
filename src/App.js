f3411ab8824ba3e096f7799a7bc1ffac2ca75f90
ExcViral <exc.viral@gmail.com>
2021-02-14 20:33:17 +0530

    Added a <Layout> JSX tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from 'react';
+import Layout from './components/Layout/Layout.js';
+
 function App() {
-	return <div className='App'>Hello</div>;
+	return (
+		<div>
+			<Layout>Hello</Layout>
+		</div>
+	);
 }
 
 export default App;
